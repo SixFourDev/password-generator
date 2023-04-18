189fcc09adb98661cc325c6a32431ce4f87dbffb
Shane <sb_94@yahoo.com>
2023-04-17 17:59:21 -0700

    added to js file

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function generatePassword() {
-  
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var passwordLength = window.prompt("How long should the password be?")
+  console.log(passwordLength);
+  var includeLowerCase = window.confirm("Make sure to include a lowercase letter.")
+  console.log(includeLowerCase);
+  var includeUpperCase = window.confirm("Make sure to include a uppercase letter.")
+  console.log(includeUpperCase);
+  var includeSpecialCharacter = window.confirm("Make sure to include a special character.")
+  console.log(includeSpecialCharacter);
+  var includeNumber = window.confirm("Make sure to include a number")
+  console.log(includeNumber);
 }
 
 
